3c1f567a5e0abf52dc185ca3ea8d082267835d55
vlasov1981@yahoo.com <77mj&)(13A>
2022-05-30 18:52:59 +0300

    30 May 2022  finish Sample properties and random variables

---
+++ week1/Measures of central tendency skill test.py
@@ -0,0 +1,20 @@
+import statistics
+
+#  q1 Consider four-elements sample (1, 2, 5, x).  Which values sample average can take for different values of x?
+#  Select all correct statements.
+
+# print(statistics.mean([1,2,5,12]))
+
+# q1
+"""x = [1, 2, 3, 4, 19]
+x_bar = statistics.mean(x)
+
+total = 0
+for i in x:
+    total += (i - x_bar)
+print(total)"""
+
+# q2 Consider sample x=(1,5,7,7). Denote elements of this sample as x1,x2,x3,x4  (i.e. x_1=1, x2=5, x3=x4=7.)
+# Find value a such that ∑4i=1(a−xi)2 is minimized.
+
+
---
+++ week1/Sample variance practice.py
@@ -0,0 +1,21 @@
+import statistics
+
+# q1 Find unbiased sample variance
+"""x = [9, 3, 8, 5, 9]
+x_mean = statistics.mean(x)
+var_unbiased = 0
+for i in x:
+    var_unbiased += (i - x_mean)**2
+var_unbiased /= (len(x)-1)
+print(var_unbiased)
+
+print(statistics.variance(x))
+"""
+# q2 What’s the right conclusion about variances v1 and v2 of these samples:
+"""x1 = [2, 8, 3, 5, 2, 5, 2, 6, 3, 5, 2, 1]
+x2 = [7, 13, 8, 10, 7, 10, 7, 11, 8, 10, 7, 6]
+print(statistics.variance(x1))
+print(statistics.variance(x2))"""
+
+# Sample variance skill test
+
